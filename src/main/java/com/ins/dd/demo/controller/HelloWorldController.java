88c614705ce94b612c2d726587c39d37480a24b2
DonVicii <donvicii09@gmail.com>
2018-05-15 01:45:37 +0800

    Initial commit

---
+++ src/main/java/com/ins/dd/demo/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.ins.dd.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
